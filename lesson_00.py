f2ef7ccf5c34af4f08e200ff753fbed578bfcfa7
Noel8008 <nsingh@brooklynsteamcenter.org>
2024-09-16 10:47:19 -0400

    9/16

---
+++ lesson_00.py
@@ -0,0 +1,25 @@
+print("Noel")
+#this is my name step 1 oms
+fav_show_=("Boondocks")
+#this is my favorite show oms step 2 
+float(100.00)
+#make a float step 3 
+int({100})
+#make a interger step 4 
+num1 = input("100")
+num2 = input("10")
+sum =int(num1) + int(num2)
+product = int(num1)* int(num2)
+print("Sum is", sum)
+print("Product is", product)
+#make the product of the two numbers from previous steps but I failed..
+("lamb_curry, pizza, baked_macaroni")
+#make favortie foods into a variable I failed aswell..
+if_
+#step 7 oms make an if statement to see if they can get their drivers license.. Im giving up.
+input("Your_name:")
+#step 8 whats yo name shawty??
+print("...bro")
+#step 9 "make a loop that prints my favortie foods" LIKE HUUH????
+print("no i dont wanna")
+#step 10 make a while loop that will never run :D
