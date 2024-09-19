8ad1e28625d6b687d4f70e607ec8a66289eeb2e5
Noel8008 <nsingh@brooklynsteamcenter.org>
2024-09-19 09:20:20 -0400

    9/19

--- 02_formatting.py
+++ 02_formatting.py
@@ -17,11 +17,12 @@ TASK 2:
 Write code that checks if a user inputs an empty string
 If the string is empty, print "invalid" otherwise print "valid"
 """
-string = input("entera a string: ")
+string = input("enter a string: ")
 if string == "":
     print("invalid")
 else:
-    print("vlaid")
+    print("valid")
+#another if else statement being used 
 
 """
 TASK 3:
@@ -29,18 +30,26 @@ TASK 3:
 Write a program that will replace the word "cat" with the word "dog"
 It should replace all occurances regardless of captilization 
 """
-
+animal = "dog"
+new_animal = animal.replace('dog' ,'cat')
+print(new_animal)
+#copied and pasted from the slides and just removed and replaced words
 
 """
 TASK 4:
 
 Write a program that takes a person's name and age as input and prints it
 """
-
-
+name = "Bartholmew"
+age = 72
+print(f"My name is {name} and I am {age} years old.")
+#thank you god for opening tasks
 """
 TASK 5:
 
 Write a program that takes in two floats, and prints the quotient
 The result should be rounded to the nearest tenth (1 decimal place)
 """
+
+#idk the beginning part </3 :'(
+print("The value is {0:.2f}".format(3.14159))
