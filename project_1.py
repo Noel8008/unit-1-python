634bcce3940d4ce22b813aa9f13192c303ff4889
Noel8008 <nsingh@brooklynsteamcenter.org>
2024-09-30 10:53:01 -0400

    9/30

--- project_1.py
+++ project_1.py
@@ -1,4 +1,6 @@
-task = []
+with open("my_file.txt") as file:
+    task = file.readlines() 
+    print(task)
 
 while True:
     print("\nOptions: ")
@@ -10,15 +12,12 @@ while True:
     choice = input("choose options 1-4: ")
 #gave my choice its variable
     if choice == "1":
-        task = input("enter task: ")
-        task.append(task)
-        print(f"task '{task}' added: ")
+        a = input("enter task: ")
+        task.append(a)
 #if statements for my option 1 
     elif choice == "2":
-        task_number = int(input("enter task choice to remove: ")) - 1
-        if 0 <= task_number < len(task):
-            removed_task = task.pop(task_number)
-            print(f"task '{removed_task} removed")
+        task_number = int(input("enter task choice to remove: "))
+        del task[task_number - 1]
 #else if option for my option 2 going more into depth with more steps of deleting a task from to do list
     elif choice == '3':
         if not task:
@@ -27,10 +26,12 @@ while True:
             print("Current tasks:")
             for index, task in enumerate(task, start=1):
                 print(f"{index}. {task}")
-#else if option for viewing your tasks
-    else:
-        print("type out a proper choice")
-#else option to see if youre typing the proper options  
+    elif choice == "4":
+        with open("my_file.txt", "w") as file:
+            task = file.writelines(task) 
+        break
+        #else if option for viewing your tasks
+        # #else option to see if youre typing the proper options  
 else:
     print("type a proper option and try again")
-#end of the code hallelujah
+    #end of the code hallelujah
